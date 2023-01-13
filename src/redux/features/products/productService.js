c9306ffc3277b869acf38ec74287b0f5e3a2ad7d
Shanu <63155782+Shanu85@users.noreply.github.com>
2023-01-13 10:07:09 +0530

    change in backend URl

--- src/redux/features/products/productService.js
+++ src/redux/features/products/productService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const BACKEND_URL=process.env.REACT_APP_BACKEND_URL
 
-const API_URL=`${BACKEND_URL}/api/products`;
+const API_URL=`${BACKEND_URL}/api/products/`;
 
 // create new product
 
@@ -26,14 +26,14 @@ const getProducts=async()=>
 // delete the product
 const deleteProduct=async(id)=>
 {
-    const response=await axios.delete(API_URL+"/"+id)
+    const response=await axios.delete(API_URL+id)
     return response.data
 }
 
 // get the product
 const getProduct=async(id)=>
 {
-    const response=await axios.get(API_URL+"/"+id)
+    const response=await axios.get(API_URL+id)
     return response.data
 }
 
